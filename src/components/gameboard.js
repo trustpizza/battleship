4ab124f531c06fc7bc23616606dfe667cd7289b2
trustpizza <maxelolsson@gmail.com>
2023-03-18 18:14:30 -0500

    Write placeShip function test

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -1,7 +1,12 @@
 export default function GameboardFactory() {
     const gameboard = {
-        board: new Array(10).fill(new Array(10).fill(null))
+        board: new Array(10).fill(new Array(10).fill(null)),
+        placeShip,
     };
+
+    function placeShip(ship, start, end) {
+
+    }
 
     return gameboard
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,4 +1,5 @@
 import GameboardFactory from "../components/gameboard";
+import ShipFactory from "../components/ship"
 
 test("Gameboard has a board: 1- arrays of 10 arrays of 10 nulls", () => {
     const gameboard = GameboardFactory();
@@ -9,6 +10,26 @@ test("Gameboard has a board: 1- arrays of 10 arrays of 10 nulls", () => {
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+    ])
+})
+
+test("Place a ship at a specific coordinate", () => {
+    const ship = ShipFactory(5);
+    const gameboard = GameboardFactory();
+
+    gameboard.placeShip(ship, [0,0], [0,4]);    
+
+    expect(gameboard.board).toEqual([
+        [ship,null,null,null,null,null,null,null,null,null],
+        [ship,null,null,null,null,null,null,null,null,null],
+        [ship,null,null,null,null,null,null,null,null,null],
+        [ship,null,null,null,null,null,null,null,null,null],
+        [ship,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
