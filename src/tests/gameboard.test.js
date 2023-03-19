347115357d144780be6173d08b83892cc379ef8d
trustpizza <maxelolsson@gmail.com>
2023-03-19 11:50:48 -0500

    Write test for vertical ship placement

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -20,7 +20,6 @@ test("Gameboard has a board: 1- arrays of 10 arrays of 10 nulls", () => {
 
 test("Place a ship at a specific coordinate horizontally", () => {
     const ship = ShipFactory(5);
-    // const ship = 1;
     const gameboard = GameboardFactory();
     gameboard.placeShip(ship, [0,0], true);    
 
@@ -29,6 +28,25 @@ test("Place a ship at a specific coordinate horizontally", () => {
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,null],
+    ])
+});
+
+test("Place a ship at a specific coordinate vertically", () => {
+    const ship = ShipFactory(4);
+    const gameboard = GameboardFactory();
+    gameboard.placeShip(ship, [0,0], false);
+
+    expect(gameboard.board).toEqual([
+        [ship,null,null,null,null,null,null,null,null,null],
+        [ship,null,null,null,null,null,null,null,null,null],
+        [ship,null,null,null,null,null,null,null,null,null],
+        [ship,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
         [null,null,null,null,null,null,null,null,null,null],
