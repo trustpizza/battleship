87ccd9e18b99b87d4a2e5d8975ddf2a2c0e98bb6
trustpizza <maxelolsson@gmail.com>
2023-03-19 10:56:42 -0500

    Placeship places a ship but the object, not the actual ship?

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -2,13 +2,26 @@ export default function GameboardFactory() {
     const gameboard = {
         board: new Array(10).fill(new Array(10).fill(null)),
         placeShip(ship, start, end) {
-            // Between this.board[start] and this.board[end] needs to be ship
-            for (let i = 1; i < ship.length; i++) {
-                
+            if (start[0] === end[0]) { // Ship is horizontal
+                const row = start[0]
+        
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[row][i] = ship
+                }
+            } else if (start[1] === end[1]) { // Ship is vertical
+                const col = start[1];
+        
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[i][col] = ship
+                }   
+            } else {
+                return new Error("Ships must be either veritically or horizontally placed")
             }
         },
     };
 
+    // Object.assign(gameboard.prototype)
 
     return gameboard
-}
+};
+
