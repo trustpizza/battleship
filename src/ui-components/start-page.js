798b28c021bf61bf001984b67eed542e1d173530
trustpizza <maxelolsson@gmail.com>
2023-03-23 10:42:41 -0500

    Create simple gameboard design for testing (not final product"

--- src/ui-components/start-page.js
+++ src/ui-components/start-page.js
@@ -1,5 +1,11 @@
 import startForm from './new-player-form';
 import PlayerFactory from "../components/player";
+import board from './board';
+// Eventually move this since there should be NO game logic here
+
+const startPage = document.createElement('div');
+//startPage.appendChild(startForm);
+startPage.append(board);
 
 startForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -8,10 +14,11 @@ startForm.addEventListener("submit", (e) => {
     const p1 = PlayerFactory(formData.get("Player 1"));
     const p2 = PlayerFactory(formData.get("Player 2"));
     // Load Page
+    //startPage.removeChild(startForm)
 });
 
-const startPage = document.createElement('div');
 
-startPage.appendChild(startForm);
+// const startBuildingBoardForm 
+
 
 export default startPage;
