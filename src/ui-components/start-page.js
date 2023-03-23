aa23c5efc3812aebf610cfe5bc61d13122b7b7b4
trustpizza <maxelolsson@gmail.com>
2023-03-22 19:26:22 -0500

    Add start page and new player forms

---
+++ src/ui-components/start-page.js
@@ -0,0 +1,9 @@
+/*
+Plan:
+Create a start-page.  The start page will have a form that requires validation to create 2 plays
+Once the two players are created it will start by showing P1s board
+Once P1 is done, it will show P2's board
+Start from here
+*/
+
+const startForm = document.createElement("form");
