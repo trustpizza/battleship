9f996b9a8faf1c5909abfe37d5c9fd0468475e5c
trustpizza <maxelolsson@gmail.com>
2023-03-24 12:08:22 -0500

    Clicking a ship location on the board updates the actual board object :)

--- src/game-logic/place-ships-board.js
+++ src/game-logic/place-ships-board.js
@@ -9,7 +9,50 @@ const currentShip = {
     },
     get () {
         return this.ship;
+    },
+    reset () {
+        this.ship = null;
     }
+};
+
+function getCellLocation(cell) {
+    const location = [];
+    const cellLocationAsArray = cell.getAttribute("location").split("")
+    cellLocationAsArray.splice(1,1);
+
+    for (let i = 0; i < cellLocationAsArray.length; i++) {
+        location.push(parseInt(cellLocationAsArray[i]))                
+    }
+    // console.log(location)
+    return location;
+}
+
+function removeShips(ships) {
+    const locationOfCurrentShip = ships.indexOf(currentShip.get());
+    ships.splice(locationOfCurrentShip, 1);
+    console.log(ships);
+}
+
+function placeShipsOnBoard(boardUI, gameboard, ships, nav) {
+    let shipsEmpty = true;
+    do {
+        boardUI.board.addEventListener('click', (e) => {
+            if (e.target.parentElement == boardUI.board) {
+                const cellLocation = getCellLocation(e.target);
+                if (currentShip.get()) {
+                    gameboard.placeShip(currentShip.get(), cellLocation, true);
+                    removeShips(ships);
+                    currentShip.reset();
+                    boardUI.updateUI();
+                    if (ships.length == 0) {
+                        shipsEmpty = true;
+                        return false;
+                    }
+                    // Remove current ship button
+                }
+            }
+        })
+    } while (!shipsEmpty)
 };
 
 const PlayerBoardBuilder = (player) => {
@@ -18,6 +61,7 @@ const PlayerBoardBuilder = (player) => {
         "flex flex-col gap-10";
 
     const boardUI = GameBoard();
+    const playerGameboard = GameboardFactory();
 
     const shipLengths = [2,3,3,4,5];
     const ships = []
@@ -25,13 +69,20 @@ const PlayerBoardBuilder = (player) => {
     for (let i = 0; i < shipLengths.length; i++) {            
         const ship = ShipFactory(shipLengths[i]);
         ships.push(ship);
-    }
+    };
 
     const nav = ShipNavFactory(ships).nav;
 
-    const playerGameboard = GameboardFactory();
+    /*
+    Plan:
+        Set the current ship
+        Once current ship is set, check if the player is clicking on the gameboard
+        If the player is clicking on the gameboard, get the location of 
+    */
 
-    container.append(boardUI, nav);
+    placeShipsOnBoard(boardUI, playerGameboard, ships, nav);
+
+    container.append(boardUI.board, nav);
     return container;
 }
 
--- src/ui-components/gameboard.js
+++ src/ui-components/gameboard.js
@@ -4,7 +4,8 @@ export default function GameBoard() {
     function cellFactory(x,y) {
         const cell = document.createElement('div');
         cell.className =
-            "border border-black h-8 w-8 md:h-20 md:w-20 bg-blue-50 hover:bg-blue-100"
+            "border border-black h-8 w-8 md:h-20 md:w-20 bg-blue-50 hover:bg-blue-100";
+        cell.setAttribute('cell', 'cell')
         cell.setAttribute("location", [x,y]);
     
         return cell;
@@ -12,13 +13,20 @@ export default function GameBoard() {
     const board = document.createElement('div');
     board.className = 
         "grid grid-rows-10 grid-cols-10";
+
+    const cells = []
+
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             const cell = cellFactory(i,j);
             board.appendChild(cell)    
+            cells.push(cell)
         }
-    }
-    return board;
+    };
+
+    // Update function
+
+    return {board, cells};
 }
 
 
